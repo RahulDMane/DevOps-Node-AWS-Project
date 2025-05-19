2445b3433e0c80d8af009b87f0ebde05e1a6b942
Rahul D Mane <rahul199023@gmail.com>
2025-05-19 16:32:32 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+{
+  "name": "node-calculator-app",
+  "version": "1.0.0",
+  "description": "Simple Node.js calculator app",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "author": "Rahul Mane",
+  "license": "RIT",
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
